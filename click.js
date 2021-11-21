d8a1a7e68f2fa1f6b1416be2d8b47c72a5268b29
Michael Hynes <mike.hynes@protonmail.com>
2021-11-21 16:19:43 -0500

    Added contentScript to capture user clicks.

--- click.js
+++ click.js
@@ -1,28 +1,33 @@
+console.log("loading click.js");
+
+document.addEventListener('click', function() {
+  console.log("Click!");
+});
+
 function isImage(i) {
   return i instanceof HTMLImageElement;
 }
 
 function clickHandler(e) {
   console.log("Click!");
-  var el = e.target.id;
-  var html = e.target.innerHTML;
-  console.log("Clicked on:" + e.target.src);
-  if (isImage(e.target)) {
-    fout = getFileName(e.target.src, 'test', '1');
-    console.log("Click: Saving " + e.target.src + " to: " + fout);
-    download(e.target.src, fout);
-  }
+  // var el = e.target.id;
+  // var html = e.target.innerHTML;
+  // console.log("Clicked on:" + e.target.src);
+  // if (isImage(e.target)) {
+  //   fout = getFileName(e.target.src, 'test', '1');
+  //   console.log("Click: Saving " + e.target.src + " to: " + fout);
+  //   download(e.target.src, fout);
+  // }
 };
 
 // document.addEventListener("click", clickHandler);
 // chrome.browserAction.onClicked.addListener(clickHandler);
 
 // ugh none of this shit works
-console.log("loading click.js");
 window.addEventListener('load', function() {
   console.log("Adding handlers");
   var imgs = document.getElementsByTagName("img");
-  console.log("Found " + imgs.lenth + " handlers");
+  console.log("Found " + imgs.length + " handlers");
   for (var i = 0; i < imgs.length; i++) {
     console.log("Adding handler to: " + imgs[i])
     imgs[i].addEventListener("click", clickHandler);
--- contentScript.js
+++ contentScript.js
@@ -1,8 +1,67 @@
 chrome.webRequest.onCompleted.addListener(contentHandler, {urls: [ "<all_urls>" ], types: ['image']}, []);
 
+// Select the node that will be observed for mutations
+// const targetNode = document.body;
+//
+// // Options for the observer (which mutations to observe)
+// const config = { attributes: true, childList: true, subtree: true };
+//
+// // Callback function to execute when mutations are observed
+// const callback = function(mutationsList, observer) {
+//     // Use traditional 'for loops' for IE 11
+//     for(const mutation of mutationsList) {
+//         if (mutation.type === 'childList') {
+//             console.log('A child node has been added or removed.');
+//         }
+//         else if (mutation.type === 'attributes') {
+//             console.log('The ' + mutation.attributeName + ' attribute was modified.');
+//         }
+//     }
+// };
+//
+// // Create an observer instance linked to the callback function
+// const observer = new MutationObserver(callback);
+//
+// // Start observing the target node for configured mutations
+// observer.observe(targetNode, config);
+
+// document.addEventListener("DOMNodeInserted", function(e) {
+//   console.log("domnodeinserted");
+//   // insertedNodes.push(e.target);
+// }, false);
+// chrome.webNavigation.onDOMContentLoaded.addListener(function () {
+//   console.log('domcontentloaded');
+//   var elems = document.body.getElementsByTagName("*");
+//   console.log(elems.length);
+// });
+// chrome.webNavigation.onBeforeNavigate.addListener(function () {
+//   console.log("Click!");
+// });
+
 function contentHandler(details){
   fout = getFileName(details.url, 'test', '0');
-  console.log("Saving " + details.url + " to: " + fout);
+  // console.log("Saving " + details.url + " to: " + fout);
+  // console.log(document.images);
+  // var search_string = "img[src*='" + details.url + "']";
+  // var element = document.querySelector(search_string);
+  // console.log(search_string);
+  // console.log(element);
   // saveAs(details.url, fout);
-  download(details.url, fout);
+  // download(details.url, fout);
 }
+
+function isImage(i) {
+  return i instanceof HTMLImageElement;
+}
+
+function clickHandler(e) {
+  console.log("Click!");
+  var el = e.target.id;
+  var html = e.target.innerHTML;
+  console.log("Clicked on:" + e.target.src);
+  if (isImage(e.target)) {
+    fout = getFileName(e.target.src, 'test', '1');
+    console.log("Click: Saving " + e.target.src + " to: " + fout);
+    download(e.target.src, fout);
+  }
+};
