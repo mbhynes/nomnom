452b128108f400806503be78b9046af1882b044d
Michael Hynes <mike.hynes@protonmail.com>
2023-03-05 23:29:25 -0500

    POC of border with responsive colouring for interactive labelling.

--- contentScript.js
+++ contentScript.js
@@ -24,11 +24,63 @@ function mutationCallback(mutations) {
   var mutation, i, len;
   for (i = 0, len = mutations.length; i < len; i++) {
     mutation = mutations[i];
-    for (const added_node of mutation.addedNodes) {
-      var all_img = added_node.getElementsByTagName('img');
-      for (const img of all_img) {
-        console.debug("Adding click listener to image: " + img.src)
-        img.addEventListener("contextmenu", (e) => clickCallback(img.src, e), false);
+    for (const node of mutation.addedNodes) {
+      try {
+        var all_img = node.getElementsByTagName('img');
+        if (all_img.length == 0) {
+          continue;
+        }
+        try {
+          // Set the pointer events of the added node to none so that the click is registered on the parent element
+          // if the style if defined
+          if (node.style) {
+            node.style.pointerEvents = "none";
+          } else {
+            // initialize the style attribute of the node
+            node.style = {pointerEvents: "none"};
+          }
+        } catch (err) {
+          console.error("Error setting pointerEvents to none for added node: " + node);
+        }
+        try {
+          var hrefs = node.getElementsByTagName('a');
+          for (const tag of hrefs) {
+            try {
+            tag.style.pointerEvents = "auto";
+            } catch (err) {
+              console.error("Error setting pointerEvents to auto for added tag: " + tag);
+            }
+          }
+        } catch (err) {
+          console.error("Error in mutationCallback: " + err);
+        }
+
+        try {
+          var all_img = node.getElementsByTagName('img');
+          for (const img of all_img) {
+            try {
+              console.debug("Adding click listener to image: " + img.src)
+              if (!img.style) {
+                img.style = {};
+              }
+              // Set the border of the img element to blue
+              img.style.padding = "none";
+              img.style.border = "5px solid blue";
+              // Set the pointer events of the image to none so that the click is registered on the parent element
+              img.style.pointerEvents = "auto";
+
+              // Set the image to turn blue on hover
+              // img.style.hover = "5px solid blue";
+              img.addEventListener("click", (e) => clickCallback(e), false);
+            } catch (err) {
+              console.error("Error adding callback to img: " + img.src);
+            }
+          }
+        } catch (err) {
+          console.error("Error in mutationCallback: " + err);
+        }
+      } catch (err) {
+        console.error("Error could not get Elements by tag name for mutated element " + err + " on: " + node);
       }
     }
   }
@@ -37,11 +89,13 @@ function mutationCallback(mutations) {
 /**
  * Handle a special right-click event on the image.
  */
-function clickCallback(src, e) {
+function clickCallback(e) {
   if (e.shiftKey) {
-    console.log("Right-clicked " + src);
     e.preventDefault();
-    chrome.runtime.sendMessage({url: src}, function(response) {
+    var img = e.target;
+    img.style.border = "5px solid red";
+    console.log("Right-clicked " + img.src);
+    chrome.runtime.sendMessage({url: img.src}, function(response) {
       console.log("Got result:", response);
     });
     return false;
